75dc4b3e1e431aaa6613731fe3a477b2efeb5b3e
KubaMiszcz <zielonyeufor@gmai.com>
2020-03-25 19:44:05 +0100

    create module and compnent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BranchNameFixerModule } from './branch-name-fixer/branch-name-fixer.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BranchNameFixerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/branch-name-fixer/branch-name-fixer/branch-name-fixer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-branch-name-fixer',
+  templateUrl: './branch-name-fixer.component.html',
+  styleUrls: ['./branch-name-fixer.component.scss']
+})
+export class BranchNameFixerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
