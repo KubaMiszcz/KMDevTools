ab429a9b7e2d7015548485d9696c7b383e51eb6d
KubaMiszcz <zielonyeufor@gmai.com>
2020-03-27 19:13:41 +0100

    rename app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'KM-DevTools'`, () => {
+  it(`should have as title 'KMDevTools'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('KM-DevTools');
+    expect(app.title).toEqual('KMDevTools');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('KM-DevTools app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('KMDevTools app is running!');
   });
 });
