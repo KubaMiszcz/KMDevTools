2173af9ebafca43efe0c824178eb961a2139bca6
KubaMiszcz <zielonyeufor@gmai.com>
2020-03-27 22:19:33 +0100

    convers works

--- src/app/guid-to-hex/guid-to-hex/guid-to-hex.component.ts
+++ src/app/guid-to-hex/guid-to-hex/guid-to-hex.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as guidToHex from 'uuid-to-hex';
+import * as hexToGuid from 'hex-to-uuid';
 
 @Component({
   selector: 'app-guid-to-hex',
@@ -6,14 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guid-to-hex.component.scss']
 })
 export class GuidToHexComponent implements OnInit {
-  guid: string;
-  hex: string;
+  guid: string = '{d3fd3540-6718-4687-956b-c8618a26e335}';
+  hex: string; // = '0xd3fd354067184687956bc8618a26e335';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  rename() { }
+  guidToHex(val: string) {
+    if (val !== undefined) {
+      this.hex = guidToHex(val.replace(/[{}]/gi, ''), false).toUpperCase();
+      console.log(this.guid, '   ===>>>   ', this.hex);
+    }
+  }
 
+  hexToGuid(val) {
+    if (val !== undefined) {
+      this.guid = hexToGuid(val, false);
+      console.log(this.hex, '   ===>>>   ', this.guid);
+    }
+
+    // const uuidStringFromHexWithLeadingZero = hexToUuid('0xd3fd354067184687956bc8618a26e335');
+    // console.log(uuidStringFromHexWithLeadingZero); //d3fd3540-6718-4687-956b-c8618a26e335
+    // this.guid = hexToGuid('0xd3fd354067184687956bc8618a26e335');
+    // console.log(this.guid);
+
+    // // let hex= uuidToHex('d3fd3540-6718-4687-956b-c8618a26e335', true);
+    // this.hex = uuidToHex(val, true);
+    // console.log(this.hex);
+  }
+
+  copyInputMessageToClipboard(inputElement) {
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
+  }
 }
