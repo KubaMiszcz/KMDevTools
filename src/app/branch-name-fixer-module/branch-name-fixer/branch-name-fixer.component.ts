225fc9028dfb57096796f10964a850fbe79522a3
KubaMiszcz <zielonyeufor@gmai.com>
2020-03-27 21:05:35 +0100

    fix events

--- src/app/branch-name-fixer-module/branch-name-fixer/branch-name-fixer.component.ts
+++ src/app/branch-name-fixer-module/branch-name-fixer/branch-name-fixer.component.ts
@@ -16,43 +16,16 @@ export class BranchNameFixerComponent implements OnInit {
   }
 
   setPrefix(val: string) {
-    console.log(val);
-
     this.prefix = val;
   }
 
   rename() {
     this.fixedBranchName = this.prefix + this.branchName
-      .replace(/[/.^;:~?*<>|\\]/gi, '')
+      .replace(/ +/gi, ' ')
+      .replace(/[/^;:~?*<>|\\]/gi, '')
+      .replace(/[._\s]*$/, '')
       .replace(/[ ]/gi, '_')
-      .replace(/,/gi, '_i_')
-      .replace(/_+/gi, '_');
+      .replace(/,/gi, '_i_');
   }
 
 }
-
-
-
-/*
- $("#branchname").on("change", function () {
-        var source = document.getElementById('branchname').value;
-        var target =
-            $('input[name=customRadio]:checked')[0].value +
-            source.replace(/ /gi, '_')
-            .replace(/,/gi, '_i_');
-        document.getElementById('fixedbranchname').value = target;
-    });
-
-    function rename(params) {
-        var source = document.getElementById('branchname').value;
-        var target =
-            $('input[name=customRadio]:checked')[0].value +
-            source.replace(/ /gi, '_')
-            .replace(/,/gi, '_i_');
-        document.getElementById('fixedbranchname').value = target;
-    }
-
-    function fixname(params) {
-        rename();
-    }
-*/
