7627da8d532b30aae76b369700b9636dc69ce1ef
Jakub Belch <JBelch@ezuniverse.com>
2020-06-04 10:07:54 +0200

    add attendance message gegnerator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BranchNameFixerModule } from './branch-name-fixer-module/branch-name-fixer.module';
+import { AttendanceComponent } from './attendance/attendance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AttendanceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/attendance/attendance.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-attendance',
+  templateUrl: './attendance.component.html',
+  styleUrls: ['./attendance.component.scss']
+})
+export class AttendanceComponent implements OnInit {
+  @ViewChild('attendanceInput') attendanceInput: ElementRef;
+  attendanceMessage: string;
+
+  fullAuto: boolean;
+  showAlert: boolean;
+
+  workDayHours = 8;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.generateAttendanceMessage();
+  }
+
+  copyInputMessageToClipboard(inputElement) {
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
+  }
+
+  generateAttendanceMessage() {
+    // [Attendance] 06/04/2020 08:05 - 16:05
+    let today = new Date();
+    this.attendanceMessage = '[Attendance] '
+      + today.getDate().toString().toString().padStart(2, "0") + '/' + today.getMonth().toString().padStart(2, "0") + '/' + today.getFullYear() + ' '
+      + today.getHours().toString().padStart(2, "0") + ':' + today.getMinutes().toString().padStart(2, "0")
+      + ' - ' + (today.getHours() + this.workDayHours).toString().padStart(2, "0") + ':' + today.getMinutes().toString().padStart(2, "0");
+  }
+}
