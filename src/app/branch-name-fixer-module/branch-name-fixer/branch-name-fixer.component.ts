c99dfbdfcf51593298d6a4a09d0df999e1483fe1
Jakub Bełch <jbelch@dtiq.com>
2020-06-24 14:30:06 +0200

    add default prefix

--- src/app/branch-name-fixer-module/branch-name-fixer/branch-name-fixer.component.ts
+++ src/app/branch-name-fixer-module/branch-name-fixer/branch-name-fixer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BranchNameFixerComponent implements OnInit {
   branchName = '';
   fixedBranchName = '';
-  prefix = '';
+  prefix = 'km-temp\\';
 
   constructor() {
 
